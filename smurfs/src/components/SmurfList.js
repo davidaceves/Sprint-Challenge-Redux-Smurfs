3300cc08e918afb1e1ff97d287cc21d2be5d4401
David <digital0david@gmail.com>
2019-04-20 12:51:28 -0400

    SmurfList

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class SmurfList extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SmurfList;
